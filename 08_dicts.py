d150766d84905571383aaf131db9e26abcd09eda
Marco Schmalz <marco@schess.ch>
2020-11-27 12:38:45 +0100

    dicts

---
+++ 08_dicts.py
@@ -0,0 +1,14 @@
+
+# In einem dict speichern wir Werte unter Stichworten ab
+person = {"vorname": "Claudius", "nachname": "Favre"}
+
+# Zugriff mit eckigen Klammer wie bei den Listen
+print(person["vorname"])
+
+# Werte können geändert werden
+person["nachname"] = "Meier"
+
+# Werte können hinzugefügt werden
+person["alter"] = 7
+
+todo = {"text": "Abwaschen", "done": False}
